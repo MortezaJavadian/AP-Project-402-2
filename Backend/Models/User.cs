3fa070a14787f70119cb7b71e5e1095b0d66df3a
AmirAbas <admin1@gmail.com>
2024-06-06 02:43:57 +0330

    Create Backend Project and add Models to it

---
+++ Backend/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Models
+{
+    public abstract class User
+    {
+        public static List<User> Users { get; set; }
+
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public static bool Exist_UserName(string username)
+            => Users.Any(user => user.UserName == username);
+    }
+}
