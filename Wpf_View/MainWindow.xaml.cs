34503103182bd6bfc23def69ac80878b2edd00d6
MortezaJavadian <mor83teza@gmail.com>
2024-06-09 01:10:17 +0330

    Completing login page design

--- Wpf_View/MainWindow.xaml.cs
+++ Wpf_View/MainWindow.xaml.cs
@@ -25,13 +25,46 @@ namespace Wpf_View
 
         private async void ShowLoginPage()
         {
-            await Task.Delay(2000);
-            for (int i = 0; i < 10; i++)
+            char[] StartText = { 'R', 'e', 's', 't', 'a', 'u', 'r', 'a', 'n', 't', ' ',
+                                 'M', 'a', 'n', 'a', 'g', 'e', 'm', 'e', 'n', 't' };
+            for (int i = 0; i < StartText.Length; i++)
             {
-
+                await Task.Delay(100);
+                StartPage.Text += StartText[i];
             }
+            
+            await Task.Delay(2000);
             StartPage.Visibility = Visibility.Collapsed;
-            LoginPage.Visibility = Visibility.Visible;
+
+            for (int j = 1; j <= 50; j++)
+            {
+                await Task.Delay(15);
+                LoginPage.Height += 7.5;
+                LoginPage.Width += 6.5;
+                LoginPage.CornerRadius = new CornerRadius(100 - 1.5 * j);
+            }
+        }
+
+        bool AllowedUsername = true;
+        bool AllowedPassword = true;
+        private void Clear_UsernameOrPassword(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (AllowedUsername && textBox.Text == "Username")
+            {
+                textBox.Text = "";
+                textBox.Foreground = Brushes.Black;
+                AllowedUsername = false;
+            }
+            else if (AllowedPassword && textBox.Text == "Password")
+            {
+                textBox.Text = "";
+                textBox.Visibility = Visibility.Collapsed;
+                LoginPasswordBox.Visibility = Visibility.Visible;
+                LoginPasswordBox.Foreground = Brushes.Black;
+                LoginPasswordBox.Focus();
+                AllowedPassword = false;
+            }
         }
     }
 }
