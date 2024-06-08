a2639af34b1a577a64850def45b45cfd439227a7
AmirAbas <admin1@gmail.com>
2024-06-08 15:03:22 +0330

    Regex and Models and create variables

--- Backend/Models/Customer.cs
+++ Backend/Models/Customer.cs
@@ -2,12 +2,66 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Backend.Models
 {
     public class Customer : User
     {
+        enum Gender
+        {
+            male,
+            female,
+        }
+        public int phoneNumber { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string Email { get; set; }
+        public string address { get; set; } // optional
+        //public Gender gender { get; set; } // optional
+        public static List<Customer> customers = new List<Customer>();  
+
+        public Customer(string username, string pass, int phone, string firstname, string lastname, string email, string addres) : base (username, pass)
+        { 
+            this.phoneNumber = phone;
+            this.firstName = firstname;
+            this.lastName = lastname;
+            this.Email = email;
+            this.address = addres;
+        }
+
+        public static bool IsValidPhonenumber(string phonenumber)
+        {
+            string pattern = @"^09\d{9}$";
+            return Regex.IsMatch(phonenumber, pattern);
+        }
         
+        public static bool IsValidName(string Name)
+        {
+            string pattern = @"^[A-Za-z]{3,32}$";
+            return Regex.IsMatch(Name, pattern);
+        }
+        
+        public static bool IsValidEmail(string email)
+        {
+            string pattern = @"^[A-Za-z]{3,32}@[A-Za-z]{3,32}\.[A-Za-z]{2,3}$";
+            return Regex.IsMatch(email, pattern);
+        } 
+        
+        public static bool IsValidPassword(string password)
+        {
+            string pattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,32}$";
+            return Regex.IsMatch(password, pattern);
+        }
+
+        public static void AddToList()
+        {
+            foreach (var user in User.Users)
+            {
+                if (user is RestaurantManager)
+                    customers.Add((Customer)user);
+            }
+        }
     }
 }
--- Backend/Models/User.cs
+++ Backend/Models/User.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Transactions;
 
 namespace Backend.Models
 {
@@ -13,7 +15,35 @@ namespace Backend.Models
         public string UserName { get; set; }
         public string Password { get; set; }
 
+        public User(string username, string pass)
+        {
+            UserName = username;
+            Password = pass;
+        }
+
         public static bool Exist_UserName(string username)
             => Users.Any(user => user.UserName == username);
+
+        public static User LoginUser(string username, string password)
+        {
+            foreach (var user in Users)
+            {
+                if (user.UserName == username)
+                {
+                    if (user.Password == password)
+                        return user;
+                    else
+                        throw new Exception($"Password for user \"{username}\"");    
+                }
+            }
+
+            throw new Exception($"There is no user with username \"{username}\" here");    
+        }
+
+        public static bool IsValid_UserName(string password)
+        {
+            string pattern = @"^(?=.*[A-Za-z].*[A-Za-z].*[A-Za-z])[A-Za-z\d]*$";
+            return Regex.IsMatch(password, pattern);
+        }
     }
 }
