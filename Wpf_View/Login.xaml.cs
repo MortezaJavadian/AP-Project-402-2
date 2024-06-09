7acfbced112a6aad0ebebc6c80003b626ef6024e
MortezaJavadian <mor83teza@gmail.com>
2024-06-09 13:09:01 +0330

    create new pages and seperate start page and login page and create sign up page

---
+++ Wpf_View/Login.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_View
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Page
+    {
+        public Login()
+        {
+            InitializeComponent();
+            StartLoginPage();
+        }
+
+        public async void StartLoginPage()
+        {
+            for (int j = 1; j <= 50; j++)
+            {
+                await Task.Delay(15);
+                LoginPage.Height += 7.5;
+                LoginPage.Width += 6.5;
+                LoginPage.CornerRadius = new CornerRadius(100 - 1.5 * j);
+            }
+        }
+
+        bool AllowedUsername = true;
+        bool AllowedPassword = true;
+        private void Clear_UsernameOrPassword(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+            if (AllowedUsername && textBox.Text == "Username")
+            {
+                textBox.Text = "";
+                textBox.Foreground = Brushes.Black;
+                AllowedUsername = false;
+            }
+            else if (AllowedPassword && textBox.Text == "Password")
+            {
+                textBox.Text = "";
+                textBox.Visibility = Visibility.Collapsed;
+                LoginPasswordBox.Visibility = Visibility.Visible;
+                LoginPasswordBox.Foreground = Brushes.Black;
+                LoginPasswordBox.Focus();
+                AllowedPassword = false;
+            }
+        }
+
+        private async void SignUpCustomer_Click(object sender, RoutedEventArgs e)
+        {
+            for (int j = 1; j <= 25; j++)
+            {
+                await Task.Delay(1);
+                LoginPage.Height -= 15;
+                LoginPage.Width -= 13;
+                LoginPage.CornerRadius = new CornerRadius(25 + 3 * j);
+            }
+            (Application.Current.MainWindow as MainWindow).Content = new SignUp();
+        }
+    }
+}
--- Wpf_View/MainWindow.xaml.cs
+++ Wpf_View/MainWindow.xaml.cs
@@ -20,10 +20,10 @@ namespace Wpf_View
         public MainWindow()
         {
             InitializeComponent();
-            ShowLoginPage();
+            ShowStartPage();
         }
 
-        private async void ShowLoginPage()
+        private async void ShowStartPage()
         {
             char[] StartText = { 'R', 'e', 's', 't', 'a', 'u', 'r', 'a', 'n', 't', ' ',
                                  'M', 'a', 'n', 'a', 'g', 'e', 'm', 'e', 'n', 't' };
@@ -32,39 +32,9 @@ namespace Wpf_View
                 await Task.Delay(100);
                 StartPage.Text += StartText[i];
             }
-            
             await Task.Delay(2000);
             StartPage.Visibility = Visibility.Collapsed;
-
-            for (int j = 1; j <= 50; j++)
-            {
-                await Task.Delay(15);
-                LoginPage.Height += 7.5;
-                LoginPage.Width += 6.5;
-                LoginPage.CornerRadius = new CornerRadius(100 - 1.5 * j);
-            }
-        }
-
-        bool AllowedUsername = true;
-        bool AllowedPassword = true;
-        private void Clear_UsernameOrPassword(object sender, RoutedEventArgs e)
-        {
-            TextBox textBox = sender as TextBox;
-            if (AllowedUsername && textBox.Text == "Username")
-            {
-                textBox.Text = "";
-                textBox.Foreground = Brushes.Black;
-                AllowedUsername = false;
-            }
-            else if (AllowedPassword && textBox.Text == "Password")
-            {
-                textBox.Text = "";
-                textBox.Visibility = Visibility.Collapsed;
-                LoginPasswordBox.Visibility = Visibility.Visible;
-                LoginPasswordBox.Foreground = Brushes.Black;
-                LoginPasswordBox.Focus();
-                AllowedPassword = false;
-            }
+            this.Content = new Login();
         }
     }
 }
